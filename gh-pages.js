0e37c90fa3f293bf69a2f462bfa47dc995aa5d08
abdullah <abdullahjawaid1999@gmail.com>
2022-06-28 22:00:53 +0800

    fix

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,7 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish(
-    'public', // path to public directory
+    'dist', // path to public directory
     {
         branch: 'gh-pages',
         repo: 'https://github.com/ajjj001/Exported-Whatsapp-Chat-Viewer.git', // Update to point to your repository
