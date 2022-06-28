34c68b6c9bb7aaf626a1d5aca7efc2f0f192e0c3
abdullah <abdullahjawaid1999@gmail.com>
2022-06-28 22:11:20 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Exported-Whatsapp-Chat-Viewer/',
   plugins: [svelte()]
 })
